a007d8c65233fc6c840221115efc71bc99472704
nischaalc <nc475@drexel.edu>
2014-05-31 15:54:10 -0400

    Added and developed custom classes

---
+++ Week10HW/Week10HW/Movie.cpp
@@ -0,0 +1,139 @@
+#include "Movies.h"
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+Movie::Movie()		//Default Constructor
+{
+}
+
+Movie::Movie(const string& title)		//Alternate Constructor
+{
+	title_ = title;
+}
+
+Movie::Movie(const string& title, const string& director, Movie_Rating rating, unsigned int year, const string& path, const vector<string>& actors)	//Alternate Constructor
+{
+	title_ = title;
+	director_ = director;
+	rating_ = rating;
+	year_ = year;
+	url_ = path;
+
+	for (int i = 0; i < actors.size(); i++)
+	{
+		actors_.push_back(actors[i]);
+	}
+
+}
+
+
+Movie::~Movie()
+{
+
+}
+
+
+string Movie::getTitle() const
+{
+	return title_;
+}
+
+string Movie::getDirector() const
+{
+	return director_;
+}
+
+Movie_Rating Movie::getRating() const
+{
+	return rating_;
+}
+
+unsigned int Movie::getYear() const
+{
+	return year_;
+}
+
+string Movie::getURL() const
+{
+	return url_;
+}
+
+int Movie::getNumActors() const
+{
+	return actors_.size();
+}
+
+void Movie::setDirector(const string& director)
+{
+	director_ = director;
+}
+
+void Movie::setRating(Movie_Rating rating)
+{
+	rating_ = rating;
+}
+
+void Movie::setYear(unsigned int year)
+{
+	year_ = year;
+}
+
+void Movie::setURL(const string& path)
+{
+	url_ = path;
+}
+
+void Movie::setTitle(const string& title)
+{
+	title_ = title;
+}
+
+void Movie::addActor(const string& actor)
+{
+	actors_.push_back(actor);
+}
+
+void Movie::output(ostream & out)			//Output to either console or file
+{
+	out << "Movie: " << title_ << endl;
+	out << "Director: " << director_ << endl;
+	out << "Year: " << year_ << endl;
+
+	if (rating_ == G)
+		out << "Rating: G" << endl;
+	else if (rating_ == PG)
+		out << "Rating: PG" << endl;
+	else if (rating_ == PG13)
+		out << "Rating: PG13" << endl;
+	else if (rating_ == R)
+		out << "Rating: R" << endl;
+	else if (rating_ == NC17)
+		out << "Rating: NC17" << endl;
+	else if (rating_ == NR)
+		out << "Rating: NR" << endl;
+
+	out << "IMDB URL: " << url_ << endl;
+	
+	int endLoop = actors_.size();
+
+	out << "Cast: " << endl;
+	for (int i = 0; i < endLoop; i++)
+	{
+		out << actors_[i] << endl;
+	}
+
+}
+
+void Movie::insert(ostream & out) const
+{
+	out << &Movie::output;
+}
+
+ostream& operator<<(ostream& out, const Movie& movieObject)
+{
+	movieObject.insert(out);
+	return out;
+}
---
+++ Week10HW/Week10HW/Movies.h
@@ -0,0 +1,113 @@
+// ----------------------------------------------------------------------------
+//
+//  Movie class
+//    Header
+//
+//  This class represents a movie, featuring a title, director, rating, 
+//  year of release, and URL for further information in the Internet
+//  Movie Database (IMDB, http://www.imdb.com ) .
+//
+// ----------------------------------------------------------------------------
+//
+//  Author:         JL Popyack
+//  Date:           4/4/4
+//  Modified:       4/2/14  -- JL Popyack, removed destructor
+//					4/8/14	-- Nischaal Cooray, added output method
+//
+// ----------------------------------------------------------------------------
+#ifndef _MOVIE__
+#define _MOVIE__
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum Movie_Rating { G, PG, PG13, R, NC17, NR };
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+class Movie
+{
+public:
+
+	// ------------------------------------------------------
+	// ----- Constructors --------------------	---------------
+	// ------------------------------------------------------
+
+	Movie();
+	Movie(const string& title);
+	Movie(const string& title,
+		const string& director,
+		Movie_Rating rating,
+		unsigned int year,
+		const string& path,
+		const vector<string>& actors);
+
+	// ------------------------------------------------------
+	// ----- Destructor -------------------------------------
+	// ------------------------------------------------------
+
+	~Movie();  // To be implemented in a future assignment.
+
+	// ------------------------------------------------------
+	// ----- Inspectors -------------------------------------
+	// ------------------------------------------------------
+
+	string getTitle() const;
+	string getDirector() const;
+	Movie_Rating getRating() const;
+	unsigned int getYear() const;
+	string getURL() const;
+	int getNumActors() const;
+	string getActorName() const;
+	
+	// ------------------------------------------------------
+	// ----- Mutators ---------------------------------------
+	// ------------------------------------------------------
+
+	void setDirector(const string& director);
+	void setRating(Movie_Rating rating);
+	void setYear(unsigned int year);
+	void setURL(const string& path);
+	void setTitle(const string& title);
+	void addActor(const string& actor);
+
+	// ------------------------------------------------------
+	// ----- Facilitators ---------------------------------------
+	// ------------------------------------------------------
+
+	void output(ostream & out);
+	void insert(ostream & out) const;
+
+	// ----------------------------------------------------------
+	// ----------------------------------------------------------
+
+private:
+	string       title_;
+	string       director_;
+	Movie_Rating rating_;
+	unsigned int year_;
+	string       url_;
+	Movie* movieLink_;
+	Actor* actorLink_;
+
+};
+
+ostream& operator<<(ostream& out, const Movie& movieObject);
+
+
+class Actor
+{
+public:
+
+
+private:
+	string actorName_;
+	Actor* actorLink_;
+
+};
+
+#endif 
